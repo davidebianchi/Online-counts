e9ab147a966cb6b867027f73205b0df19b7bc27a
Davide Bianchi <davide.bianchi@mondora.com>
2015-06-19 16:12:01 +0200

    Initial commit

---
+++ count_repo_commits_map_reduce.js
@@ -0,0 +1,30 @@
+var Promise = require("bluebird");
+var request = Promise.promisify(require("request"));
+var R = require("ramda");
+
+var orgs = ["mondora", "mondora-labs", "please-beer"];
+
+var countRepos = function (orgName) {
+    var options = {
+        method: "GET",
+        url: "https://api.github.com/orgs/" + orgName + "/repos?per_page=100",
+        json: true,
+        auth: {
+            user : "dade12",
+            password: "ca03e9df6c77d9dc9294323c23b44143b4a7cf22"
+        },
+        headers: {
+            "User-Agent": "Davide",
+        }
+    };
+    return request(options)
+        .spread(function (response, body) {
+            return body.length;
+        });
+};
+
+Promise.map(orgs, countRepos)
+    .then(R.sum)
+    .then(function (totCount) {
+        console.log("The number of repo Mondora in GitHub is: " + totCount);
+    });
