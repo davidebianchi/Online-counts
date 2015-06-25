3021bd894802de2634625fb65c7109fe623fed7b
Davide Bianchi <bianchidavide12@gmail.com>
2015-06-25 17:17:14 +0200

    Update count_repo_commits_map_reduce.js

--- count_repo_commits_map_reduce.js
+++ count_repo_commits_map_reduce.js
@@ -10,8 +10,8 @@ var countRepos = function (orgName) {
         url: "https://api.github.com/orgs/" + orgName + "/repos?per_page=100",
         json: true,
         auth: {
-            user : "davidebianchi",
-            password: "2c641a35bc16096e9fae4ec4cda681ad36315235"
+            user : "/*username*/",
+            password: "/*password access token*/"
         },
         headers: {
             "User-Agent": "Davide",
